6487249895beca69c29b627a305462bc8b2ad31a
MDE186 <mattwoodsa@gmail.com>
2023-11-21 16:17:35 +0200

    Update debian.sh

--- lxc/nginx-proxy-manager/install/debian.sh
+++ lxc/nginx-proxy-manager/install/debian.sh
@@ -105,7 +105,7 @@ runcmd apt-get update && apt-get install -y ca-certificates curl gnupg
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 runcmd NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-runcmd wget -qO - https://deb.nodesource.com/setup_20.x | bash -
+# runcmd wget -qO - https://deb.nodesource.com/setup_20.x | bash -
 runcmd apt-get install -y -q --no-install-recommends nodejs
 runcmd npm install yarn --force
 
